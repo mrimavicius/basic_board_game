a6675e755c68fbb14a58257463f97a4c803582c6
mrimavicius <mantas.rimaviciuss@gmail.com>
2022-06-19 14:26:15 +0300

    optimisation

--- script.js
+++ script.js
@@ -17,54 +17,33 @@ const board = [
     22, 21, 20, 19, 18, 17, 16, 15
 ]
 
+rollButton.onclick = () => {
+    const randomNum = Math.ceil(Math.random() * 6)
+    boardPosition += randomNum
+
+    if(boardPosition > 28) {
+        boardPosition -= 28
+        playerPoints += 200
+    }
+
+    movePlayer()
+
+    rolledNum.innerHTML = `You rolled: ${randomNum}`
+    points.innerHTML = `Your points: ${playerPoints}`
+}
+
+function movePlayer() {
+    const items = document.querySelectorAll(".item")
+
+    items.forEach(x => x.innerHTML = "")
+
+    const currentBox = document.querySelector(`.box${boardPosition}`)
+    currentBox.innerHTML = '<i class="fa-solid fa-chess-knight"></i>' 
+}
 
 function appendBoard() {
-    for(let i = 0; i < board.length; i++){
-        
-        if(board[i] > 0){
-            game.innerHTML +=
-            `<div class="show" id="${board[i]}"></div>`
-        } else {
-            game.innerHTML +=
-            `<div class="hidden"></div>`
-
-        }
-
-    }
-
-    const divs = document.querySelectorAll(".show")
-
-    divs[0].innerHTML = `
-    <i class="fa-solid fa-chess-knight"></i>
-    `
-
-    rollButton.onclick = () => {
-        const randomNumber = Math.floor(Math.random() * 6 + 1)
-        
-        boardPosition += randomNumber
-        
-        if(boardPosition > 28) {
-            boardPosition -= 28
-            playerPoints += 200
-        }
-
-        const currentDiv = Array.from(divs).find(x => Number(x.id) === boardPosition )
-
-        divs.forEach(x => x.innerHTML = '')
-
-        currentDiv.innerHTML = `
-            <i class="fa-solid fa-chess-knight"></i>
-        `
-        
-        rolledNum.innerHTML = `
-        You rolled: ${randomNumber}
-        `
-
-        points.innerHTML = `
-        Your points: ${playerPoints}
-        `
-    }
-
+    board.map(x => game.innerHTML += `<div class="item box${x}"</div>`)
+    movePlayer()
 }
 
 appendBoard()
